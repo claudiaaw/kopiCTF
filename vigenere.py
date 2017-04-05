166417f7af1c1ad21570587d8c5c25b0436e40f6
limsuixin <limsuixin@users.noreply.github.com>
2017-04-06 03:01:11 +0800

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -3,7 +3,7 @@ original=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r
 encryptionList = [['k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d','e','f','g','h','i','j'],['e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','d'],['y','z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x']]
 
 
-plaintext="hallo"
+plaintext="asdfqwerpofsdtrylookingfurtherqwperokeorkalmosttherelkasdfposdfjpasndflasdfnctfisfunapsdfkasdfrkwapung(kopictf{in53cur3})umpezmdmpzemdmpzwahadungfufcizmsaojmmmqxntygqmmmay"
 
 
 
@@ -12,14 +12,17 @@ def encrypt(text):
 	plainList = list(text)
 	ciphertext=""
 	for i in range(0,len(plainList)):
-		org = original.index(plainList[i])
-		if i%3==0:
-			ciphertext+= encryptionList[0][org]
-		elif i%3==1:
-			ciphertext+= encryptionList[1][org]
-		
+		if plainList[i].isalpha() ==True:
+			org = original.index(plainList[i])
+			if i%3==0:
+				ciphertext+= encryptionList[0][org]
+			elif i%3==1:
+				ciphertext+= encryptionList[1][org]
+			
+			else:
+				ciphertext+= encryptionList[2][org]
 		else:
-			ciphertext+= encryptionList[2][org]
+			ciphertext+=plainList[i]
 
 	return(ciphertext)
 
@@ -30,13 +33,16 @@ def decrypt(text):
 	originaltext = ""
 	position =0
 	for i in range(0,len(plainList)):
-		if i%3==0:
-			position = encryptionList[0].index(plainList[i])
-		elif i%3==1:
-			position = encryptionList[1].index(plainList[i])
+		if plainList[i].isalpha()==True:
+			if i%3==0:
+				position = encryptionList[0].index(plainList[i])
+			elif i%3==1:
+				position = encryptionList[1].index(plainList[i])
+			else:
+				position = encryptionList[2].index(plainList[i])
+			originaltext+=original[position]	
 		else:
-			position = encryptionList[2].index(plainList[i])
-		originaltext+=original[position]	
+			originaltext+=plainList[i]
 	return(originaltext)			
 	
 encrypted = encrypt(plaintext)
